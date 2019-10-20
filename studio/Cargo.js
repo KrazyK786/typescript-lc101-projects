01339db5b1297d9875f62d749b531b5347a079b1
Kristopher Johnson <krazyk786@gmail.com>
2019-10-20 16:04:27 -0500

    completed studio

---
+++ studio/Cargo.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Cargo = /** @class */ (function () {
+    function Cargo(massKg, materials) {
+        this.massKg = massKg;
+        this.materials = materials;
+    }
+    return Cargo;
+}());
+exports.Cargo = Cargo;
---
+++ studio/Cargo.ts
@@ -0,0 +1,10 @@
+import {Payload} from './Payload';
+
+export class Cargo implements Payload{
+    massKg:number;
+    materials:string;
+    constructor(massKg:number,materials:string){
+        this.massKg = massKg;
+        this.materials = materials;
+    }
+}
